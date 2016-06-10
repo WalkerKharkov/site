f0be76db01631e1ec8c1378f49d5c19d833baa57
WalkerKharkov <energaleks@gmail.com>
2016-06-10 15:04:24 +0300

    site

---
+++ php/info.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Filatov Alex web development</title>
+    
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/info_style.css">
+</head>
+<body>
+    <div class="controls">
+        <button class="btn btn_portfolio">portfolio</button>
+        <button class="btn btn_contacts">contacts</button>
+        <button class="btn btn_info">info</button>
+    </div>
+    <div class="content">
+        <div class="portfolio"></div>
+        <div class="contacts"></div>
+        <div class="info"></div>
+    </div>
+    
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.backstretch.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
